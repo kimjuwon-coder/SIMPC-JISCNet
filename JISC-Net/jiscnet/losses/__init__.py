d99fb63ca87e4cf4c024bd430f94c6789e3ce8fa
김주원 <kimjuwon1116@gmail.com>
2025-09-03 19:25:18 +0900

    Remove all files from repo

--- JISC-Net/jiscnet/losses/__init__.py
+++
@@ -1,8 +0,0 @@
-import pkgutil
-
-__all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    module_spec = loader.find_spec(module_name)
-    module = module_spec.loader.load_module(module_name)
-    exec('%s = module' % module_name)
--- README.md
+++
@@ -1,2 +0,0 @@
-# SIMPC-JISCNet
-This repository contains the official code for the paper "From Patterns to Predictions: A Shapelet-Based Framework for Directional Forecasting in Noisy Financial Time Series", presented at CIKM.
